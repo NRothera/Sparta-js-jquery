6fc7617c7936a0cacb745131303d6ae39048aed9
NRothera <31472463+NRothera@users.noreply.github.com>
2017-11-04 12:06:11 +0000

    read though, added read me

--- the-dom-starter-code/index.js
+++ the-dom-starter-code/index.js
@@ -1,5 +1,21 @@
 $(function(event){
+  var listItems = ["Go out","Come back" , "Buy food" , "Eat food", 'Eat too much', 'Feel bad for yourself'];
+
+  $(listItems).each(function(key,sentence){
+    $('#list').append($("<li>"+sentence+"</li>"))
+  })
+
+  $('li').each(function(key, sentence){
+    var list = $(listItems);
+    if (key % 2 === 0){
+      $(this).addClass('even')
+    }
+  });
+
+  $('#count').html($('list').length)
+
+  // write append loop here
 
 
-  
+  // write the class loop here
 })
